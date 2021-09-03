69834528d9bb95a8d09cf3ed72c5f208e13beaa8
mohit-sojitra <mohit.sojitra@bacancy.com>
2021-09-03 15:05:22 +0530

    drop down menu for change language added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'ng-internationalization';
+  siteLocale: string;
+  constructor(private router: Router) {
+    this.siteLocale = window.location.pathname.split('/')[1];
+    console.log(this.siteLocale);
+  }
 }
